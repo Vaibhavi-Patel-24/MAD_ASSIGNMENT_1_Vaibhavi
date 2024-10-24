024fe75012bf7d11231cd50fabd474fa71273609
Vaibhavi patel <patelvaibhavi2412@gmail.com>
2024-10-24 13:10:59 +0530

    third

--- app/src/main/java/com/example/mad_assignment_1/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment_1/MainActivity.kt
@@ -19,11 +19,13 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
 
         emptyTextView = findViewById(R.id.empty_text)
         recyclerView = findViewById(R.id.recyclerview)
